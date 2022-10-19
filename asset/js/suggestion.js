0db1e86fb17540a43720c96e953433be5ba6dc1c
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2022-10-19 14:17:34 +0200

    card

--- asset/js/suggestion.js
+++ asset/js/suggestion.js
@@ -1,18 +1,23 @@
-let suggestions = [
-    'Abyssin',
-    'American curl', 
-    'American shorthair',
-    'American wirehair',
-    'Asian',
-    'Australian mist',
-    'Bengal',
-    'Bleu russe',
-    'Bobtail américain',
-    'Bobtail des Kurilian',
-    'Bombay',
-    'British shorthair',
-    'Burmese',
-    'Burmilla',
-    'Cornish rex',
-    'Devon rex'
-]
+function suggestions()
+{
+    let suggestions = [
+        'Abyssin',
+        'American curl', 
+        'American shorthair',
+        'American wirehair',
+        'Asian',
+        'Australian mist',
+        'Bengal',
+        'Bleu russe',
+        'Bobtail américain',
+        'Bobtail des Kurilian',
+        'Bombay',
+        'British shorthair',
+        'Burmese',
+        'Burmilla',
+        'Cornish rex',
+        'Devon rex'
+    ]
+
+    return suggestions;
+}
