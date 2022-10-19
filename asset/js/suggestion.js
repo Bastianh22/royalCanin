1b5d320e823281ca3c787132924b944e3c7769e7
BriceBOKATOLA <91880016+BriceBOKATOLA@users.noreply.github.com>
2022-10-19 16:11:14 +0200

    nom des races de chat
    nom des races de chat

--- asset/js/suggestion.js
+++ asset/js/suggestion.js
@@ -1,23 +1,49 @@
-function suggestions()
-{
-    let suggestions = [
-        'Abyssin',
-        'American curl', 
-        'American shorthair',
-        'American wirehair',
-        'Asian',
-        'Australian mist',
-        'Bengal',
-        'Bleu russe',
-        'Bobtail américain',
-        'Bobtail des Kurilian',
-        'Bombay',
-        'British shorthair',
-        'Burmese',
-        'Burmilla',
-        'Cornish rex',
-        'Devon rex'
-    ]
+// function suggestions()
+// {
+//     let suggestions = [
+//         'Abyssin',
+//         'American curl', 
+//         'American shorthair',
+//         'American wirehair',
+//         'Asian',
+//         'Australian mist',
+//         'Bengal',
+//         'Bleu russe',
+//         'Bobtail américain',
+//         'Bobtail des Kurilian',
+//         'Bombay',
+//         'British shorthair',
+//         'Burmese',
+//         'Burmilla',
+//         'Cornish rex',
+//         'Devon rex'
+//     ]
 
-    return suggestions;
-}
+//     return suggestions;
+
+// }
+
+
+// tableau des noms de races de chats
+var i;  
+var sug = new Array();  
+sug[0] = "Abyssin";  
+sug[1] = "American curl";  
+sug[2] = "American shorthair";  
+sug[3] = "American wirehair";  
+sug[4] = "Asian";  
+sug[5] = "Australian mist";  
+sug[6] = "Bengal";  
+sug[7] = "Bleu russe";  
+sug[8] = "Bobtail américain";  
+sug[9] = "Bobtail des Kurilian";  
+sug[10] = "Bombay";  
+sug[11] = "British shorthair";  
+sug[12] = "Burmese";  
+sug[13] = "Burmilla";  
+sug[14] = "Cornish rex";  
+sug[15] = "Devon rex";    
+  
+for (i=0;i<sug.length;i++){  
+document.write(sug[i] + "<br>"); 
+}  
