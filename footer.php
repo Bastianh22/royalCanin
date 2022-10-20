f311ca5adb3118560f3ff3257c3774942ea523f7
Bastianh22 <81431298+Bastianh22@users.noreply.github.com>
2022-10-20 18:49:14 +0200

    amelioration index

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 
 
-        </div>
         
         <script src="asset/js/script.js"></script>
         <script src="asset/js/scearchbar.js"></script>
--- index.php
+++ index.php
@@ -15,65 +15,73 @@
 
                 <div class='cards_wrap'>
                     <div class='card_item'>
-                        <div class='card_inner'>
-                            <img class="imgProduit" src='asset/img/chat.webp'>
-                            <div class='title'>Kitten pour chaton</div>
-                            <div class='description'>
-                                <p>
-                                    Aliment sec pour chats<br>
-                                    à partir de<br>
-                                    <span id='prix'>24,99 €</span><br>
-                                    En stock
-                                </p>
+                        <a href="https://www.royalcanin.com/fr/shop/kitten-pour-chaton-2522">
+                            <div class='card_inner'>
+                                <img class="imgProduit" src='asset/img/chat.webp'>
+                                <div class='title'>Kitten pour chaton</div>
+                                <div class='description'>
+                                    <p>
+                                        Aliment sec pour chats<br>
+                                        à partir de<br>
+                                        <span id='prix'>24,99 €</span><br>
+                                        En stock
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="card_item">
-                        <div class="card_inner">
-                            <img class="imgProduit" src="asset/img/FR_129114_master.webp">
-                            <div class="title">Sterilised 37</div>
-                            <div class="description">
-                                <p>
-                                    Aliment sec pour chats<br>
+                        <a href="https://www.royalcanin.com/fr/shop/sterilised-37-2537">
+                            <div class="card_inner">
+                                <img class="imgProduit" src="asset/img/FR_129114_master.webp">
+                                <div class="title">Sterilised 37</div>
+                                <div class="description">
+                                    <p>
+                                        Aliment sec pour chats<br>
 
-                                    à partir de<br>
-                                    <span id='prix'>22,50 €</span><br>
-                                    En stock
-                                </p>
+                                        à partir de<br>
+                                        <span id='prix'>22,50 €</span><br>
+                                        En stock
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="card_item">
-                        <div class="card_inner">
-                            <img class="imgProduit" src="asset/img/FR_129088_master.webp">
-                            <div class="title">Chaton Maine Coon</div>
-                            <div class="description">
-                                <p>
-                                    Aliment sec pour chats<br>
+                        <a href="https://www.royalcanin.com/fr/shop/chaton-maine-coon-2558">
+                            <div class="card_inner">
+                                <img class="imgProduit" src="asset/img/FR_129088_master.webp">
+                                <div class="title">Chaton Maine Coon</div>
+                                <div class="description">
+                                    <p>
+                                        Aliment sec pour chats<br>
 
-                                    à partir de<br>
-                                    <span id='prix'>26,10 €</span><br>
-                                    En stock
-                                </p>
+                                        à partir de<br>
+                                        <span id='prix'>26,10 €</span><br>
+                                        En stock
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
                     <div class="card_item">
-                        <div class="card_inner">
-                            <img class="imgProduit" src="asset/img/FR_129086_master.webp">
-                            <div class="title">Chaton British Shorthair</div>
-                            <div class="description">
-                                <p>
-                                    Aliment sec pour chats<br>
+                        <a href="https://www.royalcanin.com/fr/shop/chaton-british-shorthair-2566">
+                            <div class="card_inner">
+                                <img class="imgProduit" src="asset/img/FR_129086_master.webp">
+                                <div class="title">Chaton British Shorthair</div>
+                                <div class="description">
+                                    <p>
+                                        Aliment sec pour chats<br>
 
-                                    à partir de<br>
-                                    <span id='prix'>26,10 €</span><br>
-                                    En stock
-                                </p>
+                                        à partir de<br>
+                                        <span id='prix'>26,10 €</span><br>
+                                        En stock
+                                    </p>
+                                </div>
                             </div>
-                        </div>
+                        </a>
                     </div>
-                    <p>Rodson</p>
+                </div>
 
             <?php
                 require("footer.php");
